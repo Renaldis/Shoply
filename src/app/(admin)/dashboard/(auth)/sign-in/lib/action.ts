6f6a3a612c3a1eb834918c1d3158232d4919e70a
Renaldis <renaldis142@gmail.com>
2025-02-11 22:18:18 +0700

    add useFormState, useFormStatus, SignInPage

---
+++ src/app/(admin)/dashboard/(auth)/sign-in/lib/action.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { schemaSignIn } from "@/lib/schema";
+import { ActionResult } from "@/types";
+import { redirect } from "next/navigation";
+
+export async function SignIn(
+  _: unknown,
+  formData: FormData
+): Promise<ActionResult> {
+  const validate = schemaSignIn.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validate.success) {
+    return {
+      error: validate.error?.errors[0].message,
+    };
+  }
+
+  return redirect("/dashboard/sign-in");
+}
